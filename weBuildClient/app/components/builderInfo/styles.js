5d03df36436bcaecf7eed5f87783c04666744c43
Denise Architetto <denise@digital-pioneers.com>
2017-04-03 09:19:53 -0400

    Set up the architecture: Components and Styles live in the app folder. Index page will be the pass through from index.andriod and index.ios

---
+++ weBuildClient/app/components/builderInfo/styles.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    id: {
+        textAlign: 'center',
+    },
+});
+
+module.export = styles;
---
+++ weBuildClient/app/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const map = {
+    'builderInfo' : require ('./components/builderInfo/builderInfo')
+}
+
+module.exports = map;
