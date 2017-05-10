e39a914c5c74f5756a38fb29563b9dcc9f6c99df
Denise Architetto <denise@digital-pioneers.com>
2017-05-10 17:22:58 -0400

    Add Vector Icons

---
+++ weBuildClient/app/components/jobSettings/jobSettings.js
@@ -0,0 +1,3 @@
+/**
+ * Created by DArchitetto on 5/5/17.
+ */
---
+++ weBuildClient/app/components/jobSettings/styles.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    listView: {
+        //paddingTop: 20,
+        width: '100%',
+        height: '100%'
+    },
+    community:{
+        textAlign: 'center',
+        backgroundColor: '#F6F6F9',
+
+
+    },
+    tab: {
+        top: 56,
+        elevation: 8,
+        zIndex: 5,
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#DFE2E8',
+
+    }
+});
