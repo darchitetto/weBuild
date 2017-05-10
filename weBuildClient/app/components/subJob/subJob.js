ef39ff28a50923925aae0b00becb77d64315c3e0
Denise Architetto <denise@digital-pioneers.com>
2017-05-10 18:20:48 -0400

    Get subJob working - sorta. The list is still only pulling back the first subJob

--- weBuildClient/app/components/subJob/subJob.js
+++ weBuildClient/app/components/subJob/subJob.js
@@ -1,5 +1,4 @@
 import * as Progress from 'react-native-progress';
-import jobsModel from '../../../models/jobModel';
 import React, { Component } from 'react';
 import {
     AppRegistry,
@@ -13,45 +12,29 @@ import styles from './styles'
 export default class subTask extends Component{
     constructor (){
         super();
-        this.state = {
-            dataSource: new ListView.DataSource({
-                rowHasChanged: (row1, row2) => row1 !== row2, //todo why do we need this
-            }),
-            loaded: false,
-            showSubJob: false
-        }
-    }
-
-    componentDidMount(){
-        this.fetchData();
-    }
-
-    fetchData(){
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(jobsModel.jobs),
-            loaded: true
-        });
     }
 
     render(){
         return (
-            <View style={styles.row}>
+            <View>
                 <View style={styles.row}>
-                    <View style={[styles.column, styles.subJob]}>
-                        <View style={styles.row}>
-                            <Text style={styles.topSubRow}>{this.props.category}</Text>
+                    <View style={styles.row}>
+                        <View style={[styles.column, styles.subJob]}>
+                            <View style={styles.row}>
+                                <Text style={styles.topSubRow}>{this.props.data.category}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Text style={styles.middleSubRow}>Contractor: {this.props.data.contractor}</Text>
+                            </View>
+                            <View style={styles.row}>
+                                <Text style={styles.bottomSubRow} >Start Date: {this.props.data.startDate}</Text>
+                                <Text style={styles.bottomSubRow} >Duration: {this.props.data.duration}  {this.props.data.durationType}</Text>
+                                <Progress.Bar style={styles.progress} progress={.40} width={50} height={6} />
+                            </View>
                         </View>
-                        <View style={styles.row}>
-                            <Text style={styles.middleSubRow}>Contractor: {this.props.contractor}</Text>
+                        <View style={[styles.column, styles.settings]}>
+                            <Button title=':' onPress={this.toggleSettings}/>
                         </View>
-                        <View style={styles.row}>
-                            <Text style={styles.bottomSubRow} >Start Date: {this.props.startDate}</Text>
-                            <Text style={styles.bottomSubRow} >Duration: {this.props.duration}  {this.props.durationType}</Text>
-                            <Progress.Bar style={styles.progress} progress={.40} width={50} height={6} />
-                        </View>
-                    </View>
-                    <View style={[styles.column, styles.settings]}>
-                        <Button title=':' onPress={this.toggleSettings}/>
                     </View>
                 </View>
             </View>
