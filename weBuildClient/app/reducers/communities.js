ce6dc41d83fd8ecfff9585c8da044186d5d156db
Denise Architetto <denise@digital-pioneers.com>
2017-07-23 07:09:36 -0400

    oops

--- weBuildClient/app/reducers/communities.js
+++ weBuildClient/app/reducers/communities.js
@@ -1,7 +1,7 @@
 import createReducer from '../lib/createReducer';
 import * as types from '../actions/types';
 
-export default createReducer({jobs:{}},{
+export default createReducer({communities:{}},{
         [types.SET_SEARCHED_COMMUNITIES]:(state,action) => {
             let newState = {};
             action.communities.forEach((community) => {
